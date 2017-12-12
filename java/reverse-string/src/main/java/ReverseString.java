c9c032f71313a38b4dc8abbd59204ce5c0a5a932
Abdul Khaled <abd_khaled@hotmail.com>
2017-12-12 00:05:18 +0000

    reverse string solution refactor

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -2,15 +2,7 @@ class ReverseString {
   
     String reverse(String stringToReverse) {
         //cos why not?
-        StringBuilder builder = new StringBuilder();
-        builder.append(stringToReverse);
-        return builder.reverse().toString();
-
-//        String temp = "";
-//        for(int i=stringToReverse.length()-1; i >=0; i--) {
-//            temp +=stringToReverse.charAt(i);
-//        }
-//        return temp;
+        return new StringBuilder().append(stringToReverse).reverse().toString();
     }
   
 }
